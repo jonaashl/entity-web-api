942473189c8b474180ca56f8f6e617428501d051
Jonas Lindevall <jonas.h.lindevall@gmail.com>
2023-02-28 14:04:01 +0100

    feat: Initialize first project files

---
+++ Movie Characters API/Models/Character.cs
@@ -0,0 +1,14 @@
+﻿namespace Movie_Characters_API.Models
+{
+    public class Character
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string Alias { get; set; }
+
+        public string Gender { get; set; }
+        public string Picture { get; set; } // URL to image
+
+    }
+}
---
+++ Movie Characters API/Models/DbContext.cs
@@ -0,0 +1,6 @@
+﻿namespace Movie_Characters_API.Models
+{
+    public class DbContext
+    {
+    }
+}
